440b4cfae7106a176143185f3dd65c5130fc183d
Elbekovich <jumakulovozodbek007@gmail.com>
2023-09-20 17:49:52 +0500

    database changed

--- src/SpecialTask.DataAccsess/Repositories/BaseRepository.cs
+++ src/SpecialTask.DataAccsess/Repositories/BaseRepository.cs
@@ -9,7 +9,7 @@ public class BaseRepository
     public BaseRepository()
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-
-        this._connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=exam; User Id=postgres; Password=9639;");
+        this._connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=Ozodbek; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ");
+        //this._connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=exam; User Id=postgres; Password=9639;");
     }
 }
